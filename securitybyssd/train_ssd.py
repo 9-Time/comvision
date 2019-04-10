Made some changes attempting to fix the exploding gradient, but still unable to fix it

securitybyssd/train_ssd.py
@@ -24,7 +24,7 @@ balance_data = True
 freeze_base_net = False
 freeze_net = False
 mb2_width_mult = 1.0
-lr = 0.001
+lr = 0.0000001
 momentum = 0.9
 weight_decay = 5e-4
 gamma = 0.1
@@ -33,7 +33,7 @@ extra_layers_lr = None
 scheduler = 'multi-step'
 milestones = '80,100'
 t_max = 120
-batch_size = 32
+batch_size = 16
 num_epochs = 120
 num_workers = 0
 validation_epochs = 5
@@ -208,6 +208,7 @@ if __name__ == '__main__':
 
     criterion = MultiboxLoss(config.priors, iou_threshold=0.5, neg_pos_ratio=3,
                              center_variance=0.1, size_variance=0.2, device=DEVICE)
+    torch.nn.utils.clip_grad_norm_(net.parameters(), 0.25)
     optimizer = torch.optim.SGD(params, lr=lr, momentum=momentum,
                                 weight_decay=weight_decay)
     logging.info(f"Learning rate: {lr}, Base net learning rate: {base_net_lr}, "

securitybyssd/vision/utils/box_utils.py
@@ -195,12 +195,12 @@ def hard_negative_mining(loss, labels, neg_pos_ratio):
     """
     pos_mask = labels > 0
     num_pos = pos_mask.long().sum(dim=1, keepdim=True)
-    num_neg = num_pos * neg_pos_ratio
+    num_neg = torch.clamp(neg_pos_ratio*num_pos, max=pos_mask.size(1)-1)
 
-    loss[pos_mask] = -math.inf
+    loss[pos_mask] = 0
     _, indexes = loss.sort(dim=1, descending=True)
     _, orders = indexes.sort(dim=1)
-    neg_mask = orders < num_neg
+    neg_mask = orders < num_neg.expand_as(orders)
     return pos_mask | neg_mask
 
 